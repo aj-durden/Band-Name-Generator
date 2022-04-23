6d8d303bccf4d464c700a6b1c1b50fcfdf44e1f3
Anfernee Durden <72426430+aj-durden@users.noreply.github.com>
2022-04-23 12:52:15 -0500

    Add files via upload

---
+++ Band Name Generato.py
@@ -0,0 +1,10 @@
+# This is a band namer generator! Please enjoy it! - AJD
+
+print("Welcome to the band name generator!\n")
+
+pet = input("What is your pets name?\n")
+
+city = input("Where did you grow up?\n")
+
+print("Your band name will be " + city + " " + pet )
+
